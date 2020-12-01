c9e42c8624214ea879c9f2f24f18e4b8a9e5635d
Zhentian Wan <zhentian.wan@varian.com>
2020-12-01 21:03:03 +0200

    rabbit mq

---
+++ initial-boot-app/room-cleaner-consumer/src/test/java/com/frankmoley/boot/landon/RoomCleanerConsumerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.frankmoley.boot.landon;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RoomCleanerConsumerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
